19d62f07125e7d35fc800360cddde4e255c40c21
Taylor Moog <taylormoog@vu.com>
2023-04-03 20:17:40 -0500

    put dictionary into a list, parsed through list and printed value, key, added the values to to total variable

--- displayInventory.py
+++ displayInventory.py
@@ -1,7 +1,13 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
-def displayInventory(inventory):
-    # your code goes here
+def displayInventory(stuff):
+    lst = list(stuff.keys())
+    total = 0
+    print('Inventory:')
+    for key in lst:
+        print(stuff[key], key)
+        total += stuff[key]
+    print('Total number of items: ', total)
 
 
 if __name__ == "__main__":
